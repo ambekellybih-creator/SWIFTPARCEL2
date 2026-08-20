e451673a04da73e477cc5663dd963cc419a00129
Ambe Kelly Bih <ambekellybih@gmail.com>
2026-08-20 21:41:29 +0100

    Add customer authentication

--- backend/models/Shipment.js
+++ backend/models/Shipment.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 
 const shipmentSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
     trackingNumber: {
       type: String,
       unique: true,
@@ -58,4 +64,7 @@ const shipmentSchema = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model("Shipment", shipmentSchema);
+module.exports = mongoose.model(
+  "Shipment",
+  shipmentSchema
+);
